7d6776db2aa94779da0f2414a0974d54b1496f5c
Koo <koojii@protonmail.com>
2020-09-03 22:08:14 +0300

    1.7 tehty

--- osa1/osa1.6-11/src/index.js
+++ osa1/osa1.6-11/src/index.js
@@ -17,9 +17,10 @@ const App = () => {
       <Button handler={neutralHandler} text='neutral' />
       <Button handler={badHandler} text='bad' />
       <h1>statistics</h1>
-      <StatRow name='good' amount={goodAmount} />
-      <StatRow name='neutral' amount={neutralAmount} />
-      <StatRow name='bad' amount={badAmount} />
+      <StatReport
+        goodAmount={goodAmount}
+        neutralAmount={neutralAmount}
+        badAmount={badAmount} />
     </div>
   )
 }
@@ -27,8 +28,28 @@ const App = () => {
 const Button = (props) => {
   return (<button onClick={props.handler}>{props.text}</button>)
 }
+const StatReport = ({ goodAmount, neutralAmount, badAmount }) => {
+  return (
+    <>
+      <StatRow name='good' amount={goodAmount} />
+      <StatRow name='neutral' amount={neutralAmount} />
+      <StatRow name='bad' amount={badAmount} />
+      <div>all {goodAmount + neutralAmount + badAmount}</div>
+      <Average goodAmount={goodAmount} neutralAmount={neutralAmount} badAmount={badAmount} />
+      <Percents goodAmount={goodAmount} neutralAmount={neutralAmount} badAmount={badAmount} />
+    </>
+  )
+}
 const StatRow = (props) => {
   return (<div>{props.name} {props.amount}</div>)
+}
+
+const Average = ({ goodAmount, neutralAmount, badAmount }) => {
+  let value = goodAmount * 1 + badAmount * -1
+  return (<div>average {value / (goodAmount + neutralAmount + badAmount)}</div>)
+}
+const Percents = ({ goodAmount, neutralAmount, badAmount }) => {
+  return (<div>positive {100* (goodAmount / (goodAmount + neutralAmount + badAmount))} %</div>)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
